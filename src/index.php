ff9d4f2764b379db1a9f4184c3576f30695b109c
CHBHR <chrishb.rey@gmail.com>
2022-02-23 16:50:53 +0100

    tests

--- src/index.php
+++ src/index.php
@@ -8,18 +8,4 @@
 
     require 'model/db.php';
 
-    // if(isset($_GET['action'])) {
-    //     if($_GET['action'] == 'listPost') {
-    //         listPosts();
-    //     }
-    // }elseif($_GET['action'] == 'post') {
-    //     if(isset($_GET['id']) && $_GET['id']>0){
-    //         post();
-    //     }
-    //     else{
-    //         echo "Erreur : aucun identifiant d'article envoyé";
-    //     }
-    // }
-    // else{
-    //     listPosts();
-    // }
+    
